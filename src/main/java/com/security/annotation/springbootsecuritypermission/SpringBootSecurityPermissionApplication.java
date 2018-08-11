35da442ba56a90f4150870dd849072aabcbdcd88
dilsh0d <dilshod.toj@gmail.com>
2018-08-11 09:43:23 +0500

    Init Spring Boot projects

---
+++ src/main/java/com/security/annotation/springbootsecuritypermission/SpringBootSecurityPermissionApplication.java
@@ -0,0 +1,12 @@
+package com.security.annotation.springbootsecuritypermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityPermissionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/security/annotation/springbootsecuritypermission/SpringBootSecurityPermissionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.security.annotation.springbootsecuritypermission;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSecurityPermissionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
